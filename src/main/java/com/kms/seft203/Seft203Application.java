87ea948dead6b77c3b0c36a04b9392c9f47a43b5
Dung Vo <vndung147@gmail.com>
2021-06-27 13:18:03 +0700

    fix: cors

--- src/main/java/com/kms/seft203/Seft203Application.java
+++ src/main/java/com/kms/seft203/Seft203Application.java
@@ -4,16 +4,30 @@ import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.context.annotation.Bean;
+import org.springframework.web.servlet.config.annotation.CorsRegistry;
+import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 
 @SpringBootApplication
 public class Seft203Application {
 
-	public static void main(String[] args) {
-		SpringApplication.run(Seft203Application.class, args);
-	}
+    public static void main(String[] args) {
+        SpringApplication.run(Seft203Application.class, args);
+    }
 
-	@Bean
-	CommandLineRunner runner(AppVersionRepository repo) {
-		return args -> repo.save(new AppVersion(1L, "SEFT Program", "1.0.0"));
-	}
+    @Bean
+    CommandLineRunner runner(AppVersionRepository repo) {
+        return args -> repo.save(new AppVersion(1L, "SEFT Program", "1.0.0"));
+    }
+
+    @Bean
+    public WebMvcConfigurer corsConfigurer() {
+        return new WebMvcConfigurer() {
+            @Override
+            public void addCorsMappings(CorsRegistry registry) {
+                registry.addMapping("/**")
+                        .allowedMethods("GET", "PUT", "POST", "PATCH", "DELETE", "OPTIONS")
+                        .allowedOrigins("*");
+            }
+        };
+    }
 }
