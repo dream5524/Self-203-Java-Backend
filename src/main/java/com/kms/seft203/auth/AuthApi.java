0190ace3dd7853cdc776484ac42bc0e8aa9c4bba
Dung Vo <vndung147@gmail.com>
2021-07-15 23:26:31 +0700

    fix: auth api

--- src/main/java/com/kms/seft203/auth/AuthApi.java
+++ src/main/java/com/kms/seft203/auth/AuthApi.java
@@ -1,11 +1,16 @@
 package com.kms.seft203.auth;
 
+import com.auth0.jwt.JWT;
+import com.auth0.jwt.algorithms.Algorithm;
+import com.auth0.jwt.exceptions.JWTCreationException;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.Date;
 
 @RestController
 @RequestMapping("/auth")
@@ -20,12 +25,27 @@ public class AuthApi {
     @PostMapping("/login")
     public ResponseEntity<LoginResponse> login(@RequestBody LoginRequest request) {
         if ("admin".equals(request.getUsername()) && "Admin@123".equals(request.getPassword())) {
-            LoginResponse loginResponse = new LoginResponse("<token>", "<refresh_token>");
+            LoginResponse loginResponse = new LoginResponse(createJwtToken("admin", "Admin User"), "<refresh_token>");
 
             return ResponseEntity.ok(loginResponse);
         }
 
         return ResponseEntity.status(HttpStatus.UNAUTHORIZED).build();
+    }
+
+    private String createJwtToken(String user, String displayName) {
+        try {
+            Algorithm algorithm = Algorithm.HMAC256("this is a secret");
+            return JWT.create()
+                    .withIssuer("kms")
+                    .withClaim("user", user)
+                    .withClaim("displayName", displayName)
+                    .withExpiresAt(new Date(System.currentTimeMillis() + (24 * 60 * 60 * 1000)))
+                    .sign(algorithm);
+        } catch (JWTCreationException ex) {
+            System.out.println(ex);
+            return "";
+        }
     }
 
     @PostMapping("/logout")
