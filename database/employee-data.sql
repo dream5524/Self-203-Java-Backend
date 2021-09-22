d6cf350fe5726c0beaf3af61ab315bb7e6bb86be
Huyen Mo <motran@kms-technology.com>
2021-09-22 11:28:56 +0700

    feature/30-design-database

--- database/employee-data.sql
+++ database/employee-data.sql
@@ -1,10 +1,8 @@
 
 ALTER DATABASE employee OWNER TO postgres;
-
 --
 -- Data for Name: User; Type: TABLE DATA; Schema: public; Owner: postgres
 --
-
 COPY public."User" (id, email, password, full_name) FROM stdin;
 10	ductm@gmail.com	123456	Trần Minh  Đức
 1	quannha@gmail.com	123456	Nguyễn Hoàng Anh Quân
@@ -17,12 +15,9 @@ COPY public."User" (id, email, password, full_name) FROM stdin;
 8	thidtd@gmail.com	123456	Đinh Thị Duyên Thi
 9	khangca@gmail.com	123456	Cao An Khang
 \.
-
-
 --
 -- Data for Name: Contact; Type: TABLE DATA; Schema: public; Owner: postgres
 --
-
 COPY public."Contact" (id, first_name, last_name, title, user_id, project, date_created) FROM stdin;
 32	Nguyễn Hoàng Anh	Quân	Engineering Manager	1	Implenment API to build dashboard	2021-09-18 15:57:54.880419
 33	Cao Thái	Sơn	Developer	2	Implement API to build dashboard	2021-09-18 15:57:54.880419
@@ -35,12 +30,9 @@ COPY public."Contact" (id, first_name, last_name, title, user_id, project, date_
 40	Trần Minh 	Đức	Developer	10	Implement API to build dashboard	2021-09-18 15:57:54.880419
 41	Nguyễn Quốc	Việt	Developer	3	Implement API to build dashboard	2021-09-18 15:57:54.880419
 \.
-
-
 --
 -- Data for Name: Dashboard; Type: TABLE DATA; Schema: public; Owner: postgres
 --
-
 COPY public."Dashboard" (id, title, contact_id, layout_type, list_widgets) FROM stdin;
 3	Health Information	34	Desktop	{DataTable,Text}
 4	Innovative Flexible	36	Desktop	{Text}
@@ -53,12 +45,9 @@ COPY public."Dashboard" (id, title, contact_id, layout_type, list_widgets) FROM 
 9	Intership Assignment	40	Desktop	{DataTable,Text,TodoList,''Chart}
 10	Intership Assignment	41	Desktop	{DataTable,Text,TodoList,''Chart}
 \.
-
-
 --
 -- Data for Name: Task; Type: TABLE DATA; Schema: public; Owner: postgres
 --
-
 COPY public."Task" (id, description, contact_id, date_created, is_completed) FROM stdin;
 9	Create Database	33	2021-09-18 16:12:32.147452	1
 10	Add logger	37	2021-09-18 16:12:32.147452	0
@@ -71,12 +60,9 @@ COPY public."Task" (id, description, contact_id, date_created, is_completed) FRO
 17	Assurance testing models	38	2021-09-18 16:12:32.147452	0
 18	Handle errors	41	2021-09-18 16:12:32.147452	1
 \.
-
-
 --
 -- Data for Name: Widget; Type: TABLE DATA; Schema: public; Owner: postgres
 --
-
 COPY public."Widget" (id, type, dashboard_id, min_width, min_height, configs) FROM stdin;
 4	ToDoList	7	70	70	{"type_widget":{"Text":70,"Char":70,"TodoTist":70,"DataTable":70}}
 5	Text	6	70	70	{"type_widget":{"Text":70,"Char":70,"TodoTist":70,"DataTable":70}}
@@ -116,4 +102,3 @@ COPY public."Widget" (id, type, dashboard_id, min_width, min_height, configs) FR
 7	Text	4	70	70	{"type_widget":{"Text":70,"Char":70,"TodoTist":70,"DataTable":70}}
 8	Text	3	70	70	{"type_widget":{"Text":70,"Char":70,"TodoTist":70,"DataTable":70}}
 \.
-
--- src/main/java/com/kms/seft203/Seft203Application.java
+++ src/main/java/com/kms/seft203/Seft203Application.java
@@ -29,5 +29,4 @@ public class Seft203Application {
             }
         };
     }
-
 }
