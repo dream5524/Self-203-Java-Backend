64624a4dfc6e45006125f30e0fdbe5f1fc322c08
Mo Tran <motran@kms.com.vn>
2021-09-30 12:01:40 +0700

    38-create-contact-api

--- src/main/java/com/kms/seft203/entity/Contact.java
+++ src/main/java/com/kms/seft203/entity/Contact.java
@@ -1,12 +1,14 @@
 package com.kms.seft203.entity;
 
-import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
-
-import javax.persistence.*;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+import javax.persistence.Id;
+import javax.persistence.Column;
+import javax.persistence.GenerationType;
+import javax.persistence.GeneratedValue;
 import javax.validation.constraints.NotNull;
-
 import java.time.LocalDateTime;
 
 @Data
