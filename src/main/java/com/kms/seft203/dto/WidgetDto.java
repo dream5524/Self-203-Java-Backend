d9e8a05ca8824cd0ecdadd646f1c0370edb446dc
HuyenMo <motran@kms-technology.com>
2021-11-10 23:09:55 +0700

    missing unit tét

--- src/main/java/com/kms/seft203/dto/WidgetDto.java
+++ src/main/java/com/kms/seft203/dto/WidgetDto.java
@@ -3,14 +3,16 @@ package com.kms.seft203.dto;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
+
 import java.util.Map;
 
 @Data
-@NoArgsConstructor @AllArgsConstructor
+@NoArgsConstructor
+@AllArgsConstructor
 public class WidgetDto {
     private Integer dashboard_id;
     private String type;
     private Integer minWidth;
     private Integer minHeight;
-    private Map<String, String> configs;
+    private Map<String, Object> configs;
 }
--- src/main/java/com/kms/seft203/entity/Widget.java
+++ src/main/java/com/kms/seft203/entity/Widget.java
@@ -1,34 +1,26 @@
 package com.kms.seft203.entity;
 
-import com.fasterxml.jackson.databind.annotation.JsonDeserialize;
 import lombok.AllArgsConstructor;
 import lombok.Data;
 import lombok.NoArgsConstructor;
 import org.hibernate.annotations.Type;
 import org.hibernate.annotations.TypeDef;
 import com.vladmihalcea.hibernate.type.json.JsonBinaryType;
-import org.springframework.boot.jackson.JsonComponent;
-
-import javax.persistence.CollectionTable;
 import javax.persistence.Column;
-import javax.persistence.ElementCollection;
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
 import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.JoinColumn;
 import javax.persistence.ManyToOne;
-import javax.persistence.MapKeyClass;
-import javax.persistence.MapKeyColumn;
 import javax.persistence.Table;
-import java.util.HashMap;
-import java.util.List;
 import java.util.Map;
 
 @Entity
 @Data
 @NoArgsConstructor
 @AllArgsConstructor
+@TypeDef(name = "jsonb", typeClass = JsonBinaryType.class)
 @Table(name = "widget", schema = "public")
 public class Widget {
     @Id
@@ -48,8 +40,7 @@ public class Widget {
     @JoinColumn(name = "dashboard_id", referencedColumnName = "id")
     private Dashboard dashboard;
 
-    @ElementCollection
-    @Column(name = "configs")
-   // @CollectionTable(name = "widget", joinColumns = @JoinColumn(name = "id"))
-    private Map<String, String> config;
+    @Type(type = "jsonb")
+    @Column(name = "configs", columnDefinition = "jsonb")
+    private Map<String, Object> configs;
 }
