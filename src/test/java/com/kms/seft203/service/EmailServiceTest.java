6714c50c9c922b6c6520441920b6145ce246d803
HuyenMo <motran@kms-technology.com>
2021-11-19 10:45:06 +0700

    add externalized test.properties

--- src/test/java/com/kms/seft203/service/EmailServiceTest.java
+++ src/test/java/com/kms/seft203/service/EmailServiceTest.java
@@ -4,16 +4,20 @@ import org.junit.jupiter.api.Test;
 import org.junit.runner.RunWith;
 import org.mockito.Mockito;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.context.properties.EnableConfigurationProperties;
 import org.springframework.boot.test.autoconfigure.web.servlet.AutoConfigureMockMvc;
 import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.mail.SimpleMailMessage;
 import org.springframework.mail.javamail.JavaMailSender;
+import org.springframework.test.context.TestPropertySource;
 import org.springframework.test.context.junit4.SpringRunner;
 
 @AutoConfigureMockMvc(addFilters = false)
 @RunWith(SpringRunner.class)
 @SpringBootTest
+@EnableConfigurationProperties
+@TestPropertySource(locations = "classpath:application-test.properties")
 class EmailServiceTest {
 
     @Autowired
