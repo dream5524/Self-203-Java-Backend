67da491959ce6a5d0629e04b9590213a8041b16a
HuyenMo <motran@kms-technology.com>
2021-12-02 22:35:30 +0700

    apply redis cache for app version

--- src/main/java/com/kms/seft203/controller/AppApi.java
+++ src/main/java/com/kms/seft203/controller/AppApi.java
@@ -1,24 +1,35 @@
 package com.kms.seft203.controller;
 
-import com.kms.seft203.entity.AppVersion;
 import com.kms.seft203.repository.AppVersionRepository;
 import lombok.RequiredArgsConstructor;
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.info.BuildProperties;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
+import java.io.Serializable;
+
 @RestController
 @RequestMapping("/app")
 @RequiredArgsConstructor
-public class AppApi {
+public class AppApi implements Serializable {
+    private static final Logger logger = LoggerFactory.getLogger(AuthApi.class);
     @Autowired
     private AppVersionRepository appVersionRepo;
 
+    @Autowired
+    private BuildProperties buildProperties;
+
     @GetMapping("/version")
-    public AppVersion getCurrentVersion() {
-        return appVersionRepo
-                .findById(1L)
-                .orElse(new AppVersion());
+    @Cacheable("app")
+    public String getCurrentVersion() {
+        logger.info("Get application info has begun...");
+        return "Group: " + buildProperties.getGroup() + "\n"
+                + "Name: " + buildProperties.getName() + "\n"
+                + "Version:  " + buildProperties.getVersion();
     }
 }
